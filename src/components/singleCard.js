5259019a5aea734bcc8dde9b0804b20f271209a6
Mohammad Zolghadr <mzmzolqadr@gmail.com>
2022-11-17 00:47:36 +0330

    Add files via upload

---
+++ src/components/singleCard.js
@@ -0,0 +1,24 @@
+import "./singleCard.css";
+
+const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          className="back"
+          src="/images/cover.jpg"
+          onClick={handleClick}
+          alt="card back"
+        />
+      </div>
+    </div>
+  );
+};
+export default SingleCard;
